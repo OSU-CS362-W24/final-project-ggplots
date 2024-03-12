9657944ca4616503245049f10de0eaf75dc3051c
Mason Rauschkolb <rauschkm@oregonstate.edu>
2024-03-12 15:32:05 -0700

    Implemented successful line chart generated test

--- cypress/e2e/chartGenerated.cy.js
+++ cypress/e2e/chartGenerated.cy.js
@@ -4,4 +4,21 @@ describe('navigate to site home page', () => {
   })
 })
 
+describe('navigate to line chart page and buiid a successful line chart', () => {
+  it('passes', () => {
+    cy.visit('/')
+    cy.findByRole('link', { name: 'Line' }).click()
+    cy.findByLabelText('X label').type('x')
+    cy.findByLabelText('Y label').type('y')
+    cy.findByLabelText('X').type('1')
+    cy.findByLabelText('Y').type('2')
+    cy.findByRole('button', { name: '+' }).click()
+    
+    cy.findByRole('button', { name: 'Generate chart' }).click()
+
+    // verify an image is in the dom
+    cy.findByRole('img').should('exist')
+  })
+})
+
 
